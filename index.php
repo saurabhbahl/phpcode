d2e98760cffe7699a42ad7695c231a5c9864fc88
win10 <win10@DESKTOP-GBNVV0I>
2018-06-21 13:47:15 +0530

    this is commit from Saurabh

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+class myclass {
+    
+    public function myclassfun(){
+        echo 'this is my class';
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            echo "hello world!";
+        ?>
+    </body>
+</html>
