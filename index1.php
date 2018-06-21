59f1ab1ebf776873bfa145dc4b40a6adab9301ed
win10 <win10@DESKTOP-GBNVV0I>
2018-06-21 15:51:14 +0530

    get name called

--- index1.php
+++ index1.php
@@ -7,12 +7,21 @@
  */
 class Index1{
     
-    public function  getIndex1(){
-        echo "This is index1 page";
-    }
+    public $index1;
     
-}
+    public function setIndex1($index){
+        $this->index1 = $index;
+    }
+
+
+    public function  getIndex1(){
+       return $this->index1;
+    }
+       
+}   
 
 $index1 = new Index1(); 
+
+$index1->setIndex1('REJECTED_NONFASTFORWARD');
 
 $index1->getIndex1();
