18b86a827b9fae1e237bfb360d7b798ae7cd8f96
win10 <win10@DESKTOP-GBNVV0I>
2018-06-21 15:45:09 +0530

    get name called

---
+++ index1.php
@@ -0,0 +1,18 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+class Index1{
+    
+    public function  getIndex1(){
+        echo "This is index1 page";
+    }
+    
+}
+
+$index1 = new Index1(); 
+
+$index1->getIndex1();
