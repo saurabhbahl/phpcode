fc6bfaa934c20ec6bfc9c2863e46b14067c22755
win10 <win10@DESKTOP-GBNVV0I>
2018-06-21 15:59:47 +0530

    get name called

--- index1.php
+++ index1.php
@@ -1,10 +1,5 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 class Index1{
     
     public $index1;
@@ -24,4 +19,4 @@ $index1 = new Index1();
 
 $index1->setIndex1('REJECTED_NONFASTFORWARD');
 
-$index1->getIndex1();
+echo $index1->getIndex1();
